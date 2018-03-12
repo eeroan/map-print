c9de12fc35ec2b5324fb4b15ef6180e5196e9fcf
Eero Anttila <eea@iki.fi>
2018-03-12 23:07:12 +0200

    Allow parameters

--- map-print.js
+++ map-print.js
@@ -1,6 +1,9 @@
-const center = [4173, 3155]
-const width = 15
-const height = 15
+const params = new URL(document.location).searchParams
+const x = +params.get('x') || 4173
+const y = +params.get('y') || 3155
+const width = +params.get('w') || 15
+const height = +params.get('h') || 15
+document.title = `x:${x} y:${y}, w:${width}, h:${height}`
 const createImg = (col, row) => Object.assign(document.createElement('img'), {
     src: `https://retkikartta.fi/wmts/30c616a00f157e7357721900e8b0415c/?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&` +
     `LAYER=maastokartta&STYLE=default&TILEMATRIXSET=ETRS-TM35FIN&TILEMATRIX=13&TILEROW=${row}&TILECOL=${col}&FORMAT=image%2Fpng`,
@@ -12,9 +15,9 @@ const fragment = document.createDocumentFragment()
 range(height).forEach(row => {
     fragment.appendChild(document.createElement('br'))
     range(width).forEach(col =>
-        fragment.appendChild(createImg(center[0] + col - Math.floor(width / 2), center[1] + row - Math.floor(height / 2))))
+        fragment.appendChild(createImg(x + col - Math.floor(width / 2), y + row - Math.floor(height / 2))))
 })
 
-
+new URL(document.location)
 document.getElementById('map').appendChild(fragment)
 
